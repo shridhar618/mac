1f3a8ff462cb17dfecdeaff79099ede31130f180
Shridhar Bhat <141147285+shridhar618@users.noreply.github.com>
2026-03-25 15:38:36 +0530

    ...

---
+++ javascript/doc.js
@@ -0,0 +1,9 @@
+var a=Number(prompt("What is the value of a?"));
+var b=Number(prompt("What is the value of b?"));
+var c=Number(prompt("What is the value of c?"));
+
+var add = a+b+c;
+
+alert("The sum of a, b and c is: " + add);
+
+document.write("The sum of a, b and c is: " + add);
