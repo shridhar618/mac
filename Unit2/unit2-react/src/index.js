131a882933c9716678867a8d58e48ea633acdee2
Shridhar Bhat <141147285+shridhar618@users.noreply.github.com>
2026-04-16 09:38:48 +0530

    ...

--- Unit2/unit2-react/src/index.js
+++ Unit2/unit2-react/src/index.js
@@ -4,14 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const myElement = <h1>I like JSX!</h1>;  // ✅ declare first
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(myElement);  // ✅ now it's safe
+
 reportWebVitals();
