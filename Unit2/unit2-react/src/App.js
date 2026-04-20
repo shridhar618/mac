a4a417a18c307db079fa994bd25ad01ea184e000
Shridhar Bhat <141147285+shridhar618@users.noreply.github.com>
2026-04-20 16:00:51 +0530

    ...

--- Unit2/unit2-react/src/App.js
+++ Unit2/unit2-react/src/App.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import './App.css';
-import Greeting from './GreetingFn';
-import Header from './components/Header';
+// import React from 'react';
+// import './App.css';
+// import Greeting from './GreetingFn';
+// import Header from './components/Header';
 
-function App() {
-  return (
-    <div className='App'>
-      <Header />
-      <Greeting name="Shridhar" />
-      <Greeting name="Virat" />
-      <Greeting name="Maxwell" />
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className='App'>
+//       <Header />
+//       <Greeting name="Shridhar" />
+//       <Greeting name="Virat" />
+//       <Greeting name="Maxwell" />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+import React, { Component } from 'react';
+import './App.css';
+import Greeting from './components/Greeting';
+
+class App extends Component {
+  render() {
+    const continents = [
+      'Asia',
+      'Africa',
+      'Europe',
+      'North America',
+      'South America',
+      'Australia',
+      'Antarctica',
+    ];
+
+    const helloContinent = continents.map((c, index) => (
+      <h1 key={index}>Hello {c}</h1>
+    ));
+
+    return (
+      <>
+      {helloContinent}
+      </>
+    );
+  }
 }
 
 export default App;
