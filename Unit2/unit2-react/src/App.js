f6e5bb42e1ebac965a80e897d3a881ffac9c3d31
Shridhar Bhat <141147285+shridhar618@users.noreply.github.com>
2026-04-21 09:08:37 +0530

    ...

--- Unit2/unit2-react/src/App.js
+++ Unit2/unit2-react/src/App.js
@@ -18,32 +18,87 @@
 
 
 
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+// import './App.css';
+// import Greeting from './components/Greeting';
+
+// class App extends Component {
+//   render() {
+//     const continents = [
+//       'Asia',
+//       'Africa',
+//       'Europe',
+//       'North America',
+//       'South America',
+//       'Australia',
+//       'Antarctica',
+//     ];
+
+//     const helloContinent = continents.map((c, index) => (
+//       <h1 key={index}>Hello {c}</h1>
+//     ));
+
+//     return (
+//       <>
+//       {helloContinent}
+//       </>
+//     );
+//   }
+// }
+
+// export default App;
+
+
+
+
 import './App.css';
-import Greeting from './components/Greeting';
+import React from "react";
 
-class App extends Component {
+/* Country Filter Component */
+class CountryFilter extends React.Component {
   render() {
-    const continents = [
-      'Asia',
-      'Africa',
-      'Europe',
-      'North America',
-      'South America',
-      'Australia',
-      'Antarctica',
-    ];
-
-    const helloContinent = continents.map((c, index) => (
-      <h1 key={index}>Hello {c}</h1>
-    ));
-
     return (
-      <>
-      {helloContinent}
-      </>
+      <div>This is a placeholder for the country filter.</div>
     );
   }
+}
+
+/* Country Table Component */
+class CountryTable extends React.Component {
+  render() {
+    return (
+      <div>This is a placeholder for a table of countries.</div>
+    );
+  }
+}
+
+/* Country Add Component */
+class CountryAdd extends React.Component {
+  render() {
+    return (
+      <div>This is a placeholder for a form to add a country.</div>
+    );
+  }
+}
+
+/* Main App Component */
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+        <React.Fragment>
+          <h1>Country Tracker</h1>
+
+          <CountryFilter />
+          <hr />
+
+          <CountryTable />
+          <hr />
+
+          <CountryAdd />
+        </React.Fragment>
+     </div>
+  );
 }
 
 export default App;
