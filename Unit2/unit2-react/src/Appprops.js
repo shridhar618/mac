f1d736cf40d38710a9da244d4f592870e2cc1947
Shridhar Bhat <141147285+shridhar618@users.noreply.github.com>
2026-04-22 15:09:31 +0530

    ...

--- Unit2/unit2-react/src/Appprops.js
+++ Unit2/unit2-react/src/Appprops.js
@@ -1,17 +1,21 @@
-import "./App.css";
+// import "./App.css";
 import React from "react";
 import Book from "./components/Book";
 // import BookClass from "./components/BookClass";
 
 function App() {
-  return (
-    <div style={{ border: "1px solid gray", margin: "10px", padding: "10px" }}>
-      <h1>Book Store</h1>
+    return (
+        <div style={{ border: "1px solid gray", margin: "10px", padding: "10px", color: "orange" }}>
+            <h1>Book Store</h1>
 
-      {/* <BookClass /> */}
-      <Book />
-    </div>
-  );
+            {/* <BookClass /> */}
+            <Book 
+                title="React Basics"
+                author="John Doe"
+                price={2999}
+            />
+        </div>
+    );
 }
 
 export default App;
